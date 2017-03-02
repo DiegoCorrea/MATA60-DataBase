91610ce3c88a5a6ea22db7267e1383a25fc04832
Diego Corrêa <diegocorrea.cc@gmail.com>
2017-03-02 11:23:31 -0300

    revisando lista 1

--- SQL/insertOnDB.sql
+++ SQL/insertOnDB.sql
@@ -1,33 +1,33 @@
-insert into Departamento values ('001','Marketing');
-insert into Departamento values ('002','Comercial');
-insert into Departamento values ('003','Pessoal');
-insert into Departamento values ('004','Desenvolvimento');
+INSERT INTO Departamento VALUES ('001','Marketing');
+INSERT INTO Departamento VALUES ('002','Comercial');
+INSERT INTO Departamento VALUES ('003','Pessoal');
+INSERT INTO Departamento VALUES ('004','Desenvolvimento');
 
-insert into Funcionario values (1,'Daniela','Claro','2000-04-01','222-2222', '001', null);
-insert into Funcionario values (2,'Pedro','Santos','2001-05-02','233-3333', '003', 1);
-insert into Funcionario values (3,'João','Silva','2002-06-05','333-3333', '004', null);
-insert into Funcionario values (4,'Miguel','Jardim','2000_08-01','444-4444', '004', 3);
+INSERT INTO Funcionario VALUES (1,'Daniela','Claro','2000-04-01','222-2222', '001', null);
+INSERT INTO Funcionario VALUES (2,'Pedro','Santos','2001-05-02','233-3333', '003', 1);
+INSERT INTO Funcionario VALUES (3,'João','Silva','2002-06-05','333-3333', '004', null);
+INSERT INTO Funcionario VALUES (4,'Miguel','Jardim','2000_08-01','444-4444', '004', 3);
 
-insert into Fornecedor values ('AAA', 'Empresa A');
-insert into Fornecedor values ('BBB', 'Empresa B');
-insert into Fornecedor values ('CCC', 'Empresa C');
+INSERT INTO Fornecedor VALUES ('AAA', 'Empresa A');
+INSERT INTO Fornecedor VALUES ('BBB', 'Empresa B');
+INSERT INTO Fornecedor VALUES ('CCC', 'Empresa C');
 
-insert into Comida values ('AAA', 'SL', 'Salada', 2, 0.25);
-insert into Comida values ('AAA', 'SD', 'Sopa do Dia', 1.5, null);
-insert into Comida values ('BBB', 'CG', 'Carne grelhada', 6, 0.7);
-insert into Comida values ('BBB', 'BF', 'Batata Frita', 3, null);
-insert into Comida values ('CCC', 'BF', 'Bife', 7, 1.5);
+INSERT INTO Comida VALUES ('AAA', 'SL', 'Salada', 2, 0.25);
+INSERT INTO Comida VALUES ('AAA', 'SD', 'Sopa do Dia', 1.5, null);
+INSERT INTO Comida VALUES ('BBB', 'CG', 'Carne grelhada', 6, 0.7);
+INSERT INTO Comida VALUES ('BBB', 'BF', 'Batata Frita', 3, null);
+INSERT INTO Comida VALUES ('CCC', 'BF', 'Bife', 7, 1.5);
 
-insert into Refeicao values ('01', '001', '2003-02-14', '2002-03-01');
-insert into Refeicao values ('02', '002', '2003-02-14', '2002-03-01');
-insert into Refeicao values ('03', '003', '2003-02-14', '2002-03-02');
-insert into Refeicao values ('04', '001', '2003-02-15', '2002-03-02');
-insert into Refeicao values ('05', '002', '2003-02-16', '2002-03-03');
+INSERT INTO Refeicao VALUES ('01', '001', '2003-02-14', '2002-03-01');
+INSERT INTO Refeicao VALUES ('02', '002', '2003-02-14', '2002-03-01');
+INSERT INTO Refeicao VALUES ('03', '003', '2003-02-14', '2002-03-02');
+INSERT INTO Refeicao VALUES ('04', '001', '2003-02-15', '2002-03-02');
+INSERT INTO Refeicao VALUES ('05', '002', '2003-02-16', '2002-03-03');
 
-insert into ItensRefeicao values ('01', 1, 'BBB','BF',2);
-insert into ItensRefeicao values ('01', 2, 'CCC','BF',1);
-insert into ItensRefeicao values ('02', 1, 'AAA','SL',1);
-insert into ItensRefeicao values ('02', 2, 'BBB','CG',1);
-insert into ItensRefeicao values ('03', 1, 'AAA','SL',1);
-insert into ItensRefeicao values ('04', 1, 'BBB','BF',1);
-insert into ItensRefeicao values ('04', 2, 'BBB','CG',1);
+INSERT INTO ItensRefeicao VALUES ('01', 1, 'BBB','BF',2);
+INSERT INTO ItensRefeicao VALUES ('01', 2, 'CCC','BF',1);
+INSERT INTO ItensRefeicao VALUES ('02', 1, 'AAA','SL',1);
+INSERT INTO ItensRefeicao VALUES ('02', 2, 'BBB','CG',1);
+INSERT INTO ItensRefeicao VALUES ('03', 1, 'AAA','SL',1);
+INSERT INTO ItensRefeicao VALUES ('04', 1, 'BBB','BF',1);
+INSERT INTO ItensRefeicao VALUES ('04', 2, 'BBB','CG',1);
--- SQL/lista1.sql
+++ SQL/lista1.sql
@@ -1,18 +1,39 @@
 SELECT * FROM Funcionario;
 
-SELECT nomefuncionario,sobrenmfunc FROM Funcionario;
+SELECT nomefuncionario AS nome,
+      sobrenmfunc AS sobrenome 
+FROM Funcionario;
 
-SELECT CONCAT(nomefuncionario,' ',sobrenmfunc) AS nome FROM Funcionario;
+SELECT CONCAT(nomefuncionario,' ',sobrenmfunc) AS nome 
+FROM Funcionario;
 
-SELECT descproduto,preco FROM Comida WHERE preco>=6.00;
+SELECT descproduto,
+      preco 
+FROM Comida 
+WHERE preco > 6.00;
 
-SELECT nomefuncionario, 'Funcionario Competente' AS Qualificacao FROM Funcionario;
+SELECT nomefuncionario AS nome, 
+      'Funcionario Competente' AS qualificacao 
+FROM Funcionario;
 
-SELECT CONCAT(nomefuncionario,' ',sobrenmfunc) AS nome FROM Funcionario
-WHERE CodGerente=1 OR codGerente=(SELECT codFuncionario FROM Funcionario WHERE nomefuncionario='Daniela');
+SELECT CONCAT(nomefuncionario,' ',sobrenmfunc) AS nome
+FROM Funcionario
+WHERE CodGerente = 1
+  OR codGerente =
+    (SELECT codFuncionario
+     FROM Funcionario
+     WHERE nomefuncionario='Daniela');
 
-SELECT * FROM Funcionario WHERE dtadmissao BETWEEN '2000-02-15' AND '2002-02-16';
+SELECT * 
+FROM Funcionario 
+WHERE  03 = EXTRACT(MONTH FROM dtadmissao) AND (EXTRACT(DAY FROM dtadmissao) >= 15 AND EXTRACT(DAY FROM dtadmissao) <= 16);
 
-SELECT descproduto, (preco-(preco*0.10)) AS novoValor, (preco*0.10) AS desconto FROM comida WHERE preco*0.10 <2.00;
+SELECT descproduto,
+       (preco-(preco*0.10)) AS novoValor,
+       (preco*0.10) AS desconto
+FROM comida
+WHERE preco*0.10 < 2.00;
 
-SELECT nomefuncionario FROM Funcionario WHERE codGerente is null;
+SELECT nomefuncionario AS nome
+FROM Funcionario 
+WHERE codGerente IS NULL;
